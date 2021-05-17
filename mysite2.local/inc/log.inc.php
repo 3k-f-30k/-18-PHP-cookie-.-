8bd8cd1ac77f63005cd910f2533b14bd3659683c
Ftr <73868231+3k-f-30k@users.noreply.github.com>
2021-05-17 10:18:07 +0600

    Add files via upload

---
+++ mysite2.local/inc/log.inc.php
@@ -0,0 +1,11 @@
+<?php
+$dt = date("H-i-s d-m-Y", time());
+$page = $_SERVER['REQUEST_URI'];
+$ref = $_SERVER['HTTP_REFERER'];
+$path = "$dt | $page | $ref \n";
+echo $path;
+
+$f = fopen(PATH_LOG, "a");
+fputs($f, $path);
+fclose($f);
+?>
