2a8131cb8839c4bfef5067875d2215f5c5f5ab63
Ftr <73868231+3k-f-30k@users.noreply.github.com>
2021-05-17 09:57:30 +0600

    Add files via upload

---
+++ mysite2.local/inc/cookie.inc.php
@@ -0,0 +1,13 @@
+<?php
+$visitCounter = 1;
+$lastVisit = "";
+
+if(isset($_COOKIE["visitCounter"])) {
+  $visitCounter = (int)$_COOKIE["visitCounter"];
+  $visitCounter += 1;
+} 
+
+if(isset($_COOKIE["lastVisit"])) {
+    $lastVisit = date("d-m-Y", $_COOKIE["lastVisit"]);
+}
+?>
